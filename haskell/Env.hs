ec667651a222ea4af0c4708c3713810f72661286
Leo Poulson <lp16860@my.bristol.ac.uk>
2019-07-19 13:10:35 +0100

    Remove excess in Env

--- haskell/Env.hs
+++ haskell/Env.hs
@@ -17,13 +17,8 @@ data Env = Env {inner :: Map.Map Key Value, outer :: Maybe Env}
   deriving (Eq, Show)
 
 set :: Key -> Value -> Env -> Env
-set k v@(MStr s) env
-  | lastN 10 s == "not found." = trace "nf" env
-  | otherwise                  = trace "ok" Env (Map.insert k v $ inner env) (outer env)
-set k (MErr err) env = trace err env
 set k v env = trace (show v) Env (Map.insert k v $ inner env) (outer env)
 
--- (def! x abc)
 find :: Key -> Env -> Maybe Env
 find k env = if Map.member k (inner env)
              then Just env
@@ -35,7 +30,3 @@ get k env = find k env >>= Map.lookup k . inner
 newEnv :: Env
 newEnv = Env Map.empty Nothing
 
-
--- utility
-lastN :: Int -> [a] -> [a]
-lastN n xs = foldl (const . drop 1) xs (drop n xs)
